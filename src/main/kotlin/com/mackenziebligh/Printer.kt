bd71808ce6b2fb24131af5beba27b0688d49824b
Bligh <mackenziebligh@gmail.com>
2019-10-30 09:47:22 -0700

    master: Implement a rudimentary Printer
    Printer:
    - Add print functions for MalSymbol, MalInt and MalList

--- src/main/kotlin/com/mackenziebligh/Printer.kt
+++ src/main/kotlin/com/mackenziebligh/Printer.kt
@@ -1,2 +1,13 @@
 package com.mackenziebligh
 
+/**
+ * Converts a [MalType] to a string to display on the terminal.
+ */
+fun prStr(malType: MalType): String {
+    return when (malType) {
+        is MalSymbol -> malType.value
+        is MalInt -> malType.value.toString()
+        is MalList -> malType.values.joinToString(" ", "(", ")") { prStr(it) }
+        else -> throw Exception("Cannot print $malType")
+    }
+}
