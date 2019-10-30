a80303fd53d981c47f32101c7fbad4d3ad840ad5
Bligh <mackenziebligh@gmail.com>
2019-10-30 09:56:29 -0700

    master: Implement basic Types
    Types:
    - Implement MalType abstract class for all types used by the repl
    - Implement MalList, MalInt, and MalSymbol to represent their respective types

--- src/main/kotlin/com/mackenziebligh/Types.kt
+++ src/main/kotlin/com/mackenziebligh/Types.kt
@@ -1,2 +1,22 @@
 package com.mackenziebligh
 
+abstract class MalType
+
+/**
+ * Class for containing sequential data.
+ */
+data class MalList(val values: MutableList<MalType> = mutableListOf()) : MalType() {
+    fun add(obj: MalType) {
+        this.values.add(obj)
+    }
+}
+
+/**
+ * Class for representing [Int] and [Long]
+ */
+data class MalInt(val value: Long) : MalType()
+
+/**
+ * Class for representing symbols (e.g. +, -, /, *)
+ */
+data class MalSymbol(val value: String) : MalType()
