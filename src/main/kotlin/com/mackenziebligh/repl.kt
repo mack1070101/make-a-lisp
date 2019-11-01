154d0225016e15a0e062a76ac3754adba8556e03
Bligh <mackenziebligh@gmail.com>
2019-11-01 11:57:33 -0700

    Learning session RPL
    learning-session: re-organize repl.kt
    
    repl:
    - Re organize to demo nested functions
    
    learning-session: Remove period in repl.it
    
    Move functions out of main because reasons
    
    learning-session: Minor tweaks
    
    repl:
    - Tweak loop function signature

--- src/main/kotlin/com/mackenziebligh/repl.kt
+++ src/main/kotlin/com/mackenziebligh/repl.kt
@@ -4,67 +4,49 @@ package com.mackenziebligh
  * Singleton that maps [String] to a corresponding [MalFunction].
  */
 private object REPL {
-    val env = mapOf(
-        "+" to MalFunction { values: List<MalType> -> values.reduceRight { x, y -> (x as MalLong) + (y as MalLong) } },
-        "-" to MalFunction { values: List<MalType> -> values.reduceRight { x, y -> (x as MalLong) - (y as MalLong) } },
-        "/" to MalFunction { values: List<MalType> -> values.reduceRight { x, y -> (x as MalLong) / (y as MalLong) } },
-        "*" to MalFunction { values: List<MalType> -> values.reduceRight { x, y -> (x as MalLong) * (y as MalLong) } })
+    val env = mapOf<String, MalFunction> ()
 }
 
 /**
  * Reads input from the console and converts it into a [MalType]. This is the first function called, and the
  * result is passed off to [eval].
+ *
+ * TODO Step 0: Implement this function using code from this repository and without declaring types
  */
-fun read(input: String) = readStr(input)
+fun read(input: String) = TODO() as MalType
 
 /**
  * Evaluates a [MalType] to it's resulting type. This function is called after [read], and the result is passed off to
  * [printer].
  */
-fun eval(ast: MalType, replEnv: Map<String, MalFunction> = REPL.env): MalType =
-    if (ast is MalList && ast.values.isNotEmpty()) {
-        val evaluated = evalAst(ast, replEnv) as MalList
-        if (evaluated.first() !is MalFunction) {
-            evaluated
-        } else  {
-            (evaluated.first() as MalFunction).invoke(evaluated.rest())
-        }
-    }  else {
-        evalAst(ast, replEnv)
-    }
-
-/**
- * Helper function to evaluate an abstract syntax tree. Called by [eval].
- */
-fun evalAst(malAst: MalType, replEnv: Map<String, MalFunction>) =
-    when (malAst) {
-        is MalSymbol -> replEnv[malAst.value] ?: throw Exception("${malAst.value} not found")
-        is MalList -> malAst.values.fold(MalList(), { a, b -> a.add(eval(b, replEnv)); a })
-        else -> malAst
-    }
+fun eval(ast: MalType, replEnv: Map<String, MalFunction> = REPL.env): MalType = ast
 
 /**
  * Converts a [MalType] into a printable string. Called after [eval] and before [loop].
+ *
+ * TODO Step 0: Implement this function using code from this repository and without declaring types
  */
-fun printer(input: MalType) = prStr(input)
+fun printer(input: MalType) = TODO() as String
 
 /**
- * Loops infinitely via recursion. Invokes a "ReadEvaluatePrint" (rep) function with associated command line UI.
+ * Creates an infinite loop via magic.
+ *
+ * TODO Step 1: Implement a function that loops infinitely and calls param "rep" once per loop
  */
-fun loop(rep: () -> String) {
+fun loop(rep: () -> Any?) {
     print("> ")
-    println(rep.invoke())
-    loop(rep)
+    TODO("Implement the rest of loop here. readLine() may be helpful")
 }
 
 /**
  * Runs a "ReadEvaluatePrint" (rep) function infinitely via [loop].
+ *
+ * TODO Step 2: Implement a REPL here
  */
 fun main(args: Array<String>) = loop {
-    try {
-        printer(eval(read(readLine() ?: ""), REPL.env))
-    }  catch (e: Exception)  {
-    e.printStackTrace()
-    ""
+        try {
+            TODO("Implement your read eval printer step here")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
-}
